a0732f5d7bff23643b77e18697d3ecaaf0ecc149
Jijo T Koshy <jijotkoshy@gmail.com>
2018-12-13 12:07:15 +0530

    first commit

---
+++ server_self_run.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+
+app = Flask(__name__)
+port = '5000'
+
+@app.route('/', methods=['GET'])
+def index():
+  # FETCH THE View Name
+  view_name = 'Software%20Change'
+  # FETCH job details
+  getURL = r"http://spc-jenkins.mo.sap.corp/view/"+view_name+"/api/json?tree=jobs[name]"
+  print(getURL)
+  r = requests.get(getURL)
+  #http://mo-4fa780a4e:8080/view/Software%20Change/api/json?pretty=true&tree=jobs[name,color] jijo
+  return jsonify(
+    status=r.status_code,
+    replies=[{
+        'type': 'text',
+        'content': r.json()['jobs']
+    }]
+  )
+
+
+
+@app.route('/errors', methods=['POST'])
+def errors():
+  print(json.loads(request.get_data()))
+  return jsonify(status=200)
+
+app.run(port=port)
